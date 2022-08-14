e90ac088bc74bcaa7ee911738d107cbee50e3309
Will Bang <willhbang@gmail.com>
2022-08-14 19:19:41 -0400

    accordion

---
+++ web/src/components/Accordion/Accordion.stories.tsx
@@ -0,0 +1,7 @@
+import Accordion from './Accordion'
+
+export const generated = (args) => {
+  return <Accordion {...args} />
+}
+
+export default { title: 'Components/Accordion' }
---
+++ web/src/components/Accordion/Accordion.tsx
@@ -0,0 +1,34 @@
+import _Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const Accordion = () => {
+  return (
+    <div>
+      <_Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+          sx={{
+            '& .MuiAccordionSummary-expandIconWrapper': {
+              color: 'red'
+            }
+          }}
+        >
+          <Typography>Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </_Accordion>
+    </div>
+  )
+}
+
+export default Accordion
